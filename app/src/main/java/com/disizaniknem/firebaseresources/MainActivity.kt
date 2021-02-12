1e5293807be25de69e94ff0436f2e1eeaa467b26
disizaniknem <disizaniknem@gmail.com>
2021-02-12 23:12:36 +0100

    initial commit

---
+++ app/src/main/java/com/disizaniknem/firebaseresources/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.disizaniknem.firebaseresources
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
