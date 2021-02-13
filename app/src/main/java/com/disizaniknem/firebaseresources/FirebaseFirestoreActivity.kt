31eb26fd27475c841709b527fafc686ac08a67d7
disizaniknem <disizaniknem@gmail.com>
2021-02-13 01:16:17 +0100

    Création layout pour Firebase Firestore

---
+++ app/src/main/java/com/disizaniknem/firebaseresources/FirebaseFirestoreActivity.kt
@@ -0,0 +1,11 @@
+package com.disizaniknem.firebaseresources
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirebaseFirestoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firebase_firestore)
+    }
+}
